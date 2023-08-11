a5aeed45886df28be49077b51c6e9ac62e4045dc
Davide Ferracin <davide.ferracin@protonmail.com>
2023-08-11 14:30:43 +0200

    Add “normal” MC script for spinful SIAM

---
+++ siam/spinful/mc.jl
@@ -0,0 +1,168 @@
+using ITensors
+using ITensors.HDF5
+using DelimitedFiles
+using PseudomodesTTEDOPA
+using TimeEvoVecMPS
+
+let
+    parameters = load_pars(ARGS[1])
+
+    # Input: system parameters
+    # ------------------------
+    system_initstate = parameters["sys_ini"]
+    system_length = 1
+    ε = parameters["sys_en"]
+    U = parameters["spin_interaction"]
+
+    # Input: chain stub parameters
+    # ----------------------------
+    thermofield_coefficients = readdlm(
+        parameters["thermofield_coefficients"], ',', Float64; skipstart=1
+    )
+    empty_chain_coups = thermofield_coefficients[:, 1]
+    empty_chain_freqs = thermofield_coefficients[:, 3]
+    filled_chain_coups = thermofield_coefficients[:, 2]
+    filled_chain_freqs = thermofield_coefficients[:, 4]
+    chain_length = parameters["chain_length"]
+
+    # Input: closure parameters
+    # -------------------------
+    empty_Ω = parameters["empty_asympt_frequency"]
+    empty_K = parameters["empty_asympt_coupling"]
+    filled_Ω = parameters["filled_asympt_frequency"]
+    filled_K = parameters["filled_asympt_coupling"]
+
+    α = readdlm(parameters["MC_alphas"]) # α[l,1] = Re(αₗ) and α[l,2] = Im(αₗ)...
+    β = readdlm(parameters["MC_betas"])
+    w = readdlm(parameters["MC_coups"])
+
+    emptymc = closure(empty_Ω, empty_K, α, β, w)
+    filledmc = closure(filled_Ω, filled_K, α, β, w)
+    closure_length = length(emptymc)
+
+    total_size = system_length + 2chain_length + 2closure_length
+
+    # Site ranges
+    system_site = 1
+    empty_chain_range = range(; start=2, step=2, length=chain_length)
+    empty_closure_range = range(;
+        start=empty_chain_range[end] + 2, step=2, length=closure_length
+    )
+    filled_chain_range = range(; start=3, step=2, length=chain_length)
+    filled_closure_range = range(;
+        start=filled_chain_range[end] + 2, step=2, length=closure_length
+    )
+    @assert filled_closure_range[end] == total_size
+
+    initstate_file = get(parameters, "initial_state_file", nothing)
+    if isnothing(initstate_file)
+        #system_initstate = parameters["sys_ini"]
+        sites = siteinds("vElectron", total_size)
+        initialsites = Dict(
+            [
+                system_site => parameters["sys_ini"]
+                [st => "UpDn" for st in filled_chain_range]
+                [st => "UpDn" for st in filled_closure_range]
+                [st => "Emp" for st in empty_chain_range]
+                [st => "Emp" for st in empty_closure_range]
+            ],
+        )
+        vecρₜ = MPS(sites, [initialsites[i] for i in 1:total_size])
+        start_from_file = false
+    else
+        vecρₜ = h5open(initstate_file, "r") do file
+            return read(file, parameters["initial_state_label"], MPS)
+        end
+        sites = siteinds(vecρₜ)
+        start_from_file = true
+        # We need to extract the site indices from vecρₜ or else, if we define them from
+        # scratch, they will have different IDs and they won't contract correctly.
+    end
+
+    L = MPO(
+        ε * gkslcommutator("Ntot", system_site) +
+        U * gkslcommutator("NupNdn", system_site) +
+        empty_chain_coups[1] *
+        exchange_interaction(sites[system_site], sites[empty_chain_range[1]]) +
+        filled_chain_coups[1] *
+        exchange_interaction(sites[system_site], sites[filled_chain_range[1]]) +
+        spin_chain(
+            empty_chain_freqs[1:chain_length],
+            empty_chain_coups[2:chain_length],
+            sites[empty_chain_range],
+        ) +
+        spin_chain(
+            filled_chain_freqs[1:chain_length],
+            filled_chain_coups[2:chain_length],
+            sites[filled_chain_range],
+        ) +
+        closure_op(emptymc, sites[empty_closure_range], empty_chain_range[end]) +
+        filled_closure_op(filledmc, sites[filled_closure_range], filled_chain_range[end]),
+        sites,
+    )
+
+    timestep = parameters["tstep"]
+    tmax = parameters["tmax"]
+
+    obs = []
+    oblist = parameters["observables"]
+    for key in keys(oblist)
+        foreach(i -> push!(obs, [key, i]), oblist[key])
+    end
+
+    cb = LocalPosVecMeasurementCallback(
+        createObs(obs), sites, parameters["ms_stride"] * timestep
+    )
+
+    if get(parameters, "convergence_factor_bondadapt", 0) == 0
+        @info "Using standard algorithm."
+        if !start_from_file
+            growMPS!(vecρₜ, parameters["max_bond"])
+        end
+        tdvp1vec!(
+            vecρₜ,
+            L,
+            timestep,
+            tmax,
+            sites;
+            hermitian=false,
+            normalize=false,
+            callback=cb,
+            progress=true,
+            exp_tol=parameters["exp_tol"],
+            krylovdim=parameters["krylov_dim"],
+            store_psi0=false,
+            io_file=parameters["out_file"],
+            io_ranks=parameters["ranks_file"],
+            io_times=parameters["times_file"],
+        )
+    else
+        @info "Using adaptive algorithm."
+        if !start_from_file
+            growMPS!(vecρₜ, 4)
+        end
+        adaptivetdvp1vec!(
+            vecρₜ,
+            L,
+            timestep,
+            tmax,
+            sites;
+            hermitian=false,
+            normalize=false,
+            callback=cb,
+            progress=true,
+            exp_tol=parameters["exp_tol"],
+            krylovdim=parameters["krylov_dim"],
+            store_psi0=false,
+            io_file=parameters["out_file"],
+            io_ranks=parameters["ranks_file"],
+            io_times=parameters["times_file"],
+            convergence_factor_bonddims=parameters["convergence_factor_bondadapt"],
+            max_bond=parameters["max_bond"],
+        )
+    end
+
+    f = h5open(parameters["state_file"], "w")
+    write(f, "final_state", vecρₜ)
+    close(f)
+end
