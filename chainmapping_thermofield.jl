c788152f978d59216c65e0a1a14d561e3c940c47
Davide Ferracin <davide.ferracin@protonmail.com>
2023-11-01 20:45:19 +0100

    Add merging feature to thermofield script
    Rewrite the thermofield script so that it can handle multiple
    environments and merge them into just two (then the usual chain mapping
    is applied), the usual empty and filled ones.
    The old thermofield script has been kept (as
    `chainmapping_thermofield_single_env`) for compatibility with the old
    JSON parameter files.

--- chainmapping_thermofield.jl
+++ chainmapping_thermofield.jl
@@ -3,60 +3,69 @@
 using DelimitedFiles, JSON
 using PseudomodesTTEDOPA
 
-disablegrifqtech()
-
 let
     # Load information on the spectral density from file.
     file = ARGS[1]
-    local p
-    open(file) do inp
+
+    p = open(file) do inp
         s = read(inp, String)
-        p = JSON.parse(s)
+        return JSON.parse(s)
     end
     sd_info = merge(p, Dict("filename" => file))
 
     # Parse the spectral density function given in the info file.
-    fn = sd_info["spectral_density"]
-    tmp = eval(Meta.parse("(a, x) -> " * fn))
-    sdf = x -> tmp(sd_info["parameters"], x)
-    T = sd_info["temperature"]
-    μ = sd_info["chemical_potential"]
-    domain = sd_info["domain"]
+    sdfs = []
+    for d in sd_info["environments"]
+        tmp = eval(Meta.parse("(a, x) -> " * d["spectral_density"]))
+        push!(sdfs, x -> tmp(d["parameters"], x))
+    end
+    Ts = [d["temperature"] for d in sd_info["environments"]]
+    μs = [d["chemical_potential"] for d in sd_info["environments"]]
+    domains = [d["domain"] for d in sd_info["environments"]]
+
     chain_length = sd_info["number_of_oscillators"]
 
-    # From the original spectral density J: Ω -> [0, +∞) we create two new environments
-    # with spectral densities J₊: Ω -> [0, +∞) and J₋: Ω -> [0, +∞) given by
-    #   J₊(ω) = [1 - n(ω)] J(ω),
-    #   J₋(ω) = n(ω) J(ω)
-    # defined on Ω.
-    # We use the standard TEDOPA chain mapping to transform these two environments into
-    # two linear chains: J₊ will be associated to an initially empty chain, and J₋ to an
-    # initially filled chain.
-
-    if T == 0
-        # Even though Julia is able to handle T = 0 in the formulae, we still need to
-        # intervene to manually restrict the domain so that the part where the transformed
-        # spectral densities are identically zero are removed.
-        domainempty = (μ, filter(>(μ), domain)...)
-        domainfilled = (filter(<(μ), domain)..., μ)
-    else
-        domainempty = domain
-        domainfilled = domain
+    domains_empty = []
+    domains_filled = []
+    for (T, μ, domain) in zip(Ts, μs, domains)
+        if T == 0
+            # Even though Julia is able to handle T = 0 in the formulae, we still need to
+            # intervene to manually restrict the domain so that the part where the transformed
+            # spectral densities are identically zero are removed.
+            push!(domains_empty, [μ, filter(>(μ), domain)...])
+            push!(domains_filled, [filter(<(μ), domain)..., μ])
+        else
+            push!(domains_empty, domain)
+            push!(domains_filled, domain)
+        end
     end
 
     n(β, μ, ω) = (exp(β * (ω - μ)) + 1)^(-1)
-    sdfempty(ω) = (1 - n(1 / T, μ, ω)) * sdf(ω)
-    sdffilled(ω) = n(1 / T, μ, ω) * sdf(ω)
+
+    function merged_sdfempty(ω)
+        return sum([(1 - n(1 / T, μ, ω)) * f(ω) for (f, T, μ) in zip(sdfs, Ts, μs)])
+    end
+    function merged_sdffilled(ω)
+        return sum([n(1 / T, μ, ω) * f(ω) for (f, T, μ) in zip(sdfs, Ts, μs)])
+    end
+
+    # To merge the domains: concatenate, sort, then remove duplicates
+    merge_domains(domains) = unique(sort(vcat(domains...)))
 
     (freqempty, coupempty, sysintempty) = chainmapcoefficients(
-        sdfempty, domainempty, chain_length - 1; Nquad=sd_info["PolyChaos_nquad"]
+        merged_sdfempty,
+        merge_domains(domains_empty),
+        chain_length - 1;
+        Nquad=sd_info["PolyChaos_nquad"],
     )
     (freqfilled, coupfilled, sysintfilled) = chainmapcoefficients(
-        sdffilled, domainfilled, chain_length - 1; Nquad=sd_info["PolyChaos_nquad"]
+        merged_sdffilled,
+        merge_domains(domains_filled),
+        chain_length - 1;
+        Nquad=sd_info["PolyChaos_nquad"],
     )
-    # Subtract the chemical potential from the frequencies.
-    freqfilled .-= μ
-    freqempty .-= μ
+    # We assume that the energies in the free environment Hamiltonians have already
+    # been shifted with the chemical potentials. We won't do that here.
 
     open(replace(sd_info["filename"], ".json" => ".thermofield"), "w") do output
         writedlm(output, ["coupempty" "coupfilled" "freqempty" "freqfilled"], ',')
