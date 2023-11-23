ecc6be52e5d75905c9fa2b559c75e6803b7c22d3
Davide Ferracin <davide.ferracin@protonmail.com>
2023-11-23 15:57:10 +0100

    Move `localoperator.jl` after `utils.jl`
    Switch the call order of the two files within `TimeEvoVecMPS.jl` so that
    `localoperator.jl` can see the definitions of the TDVP types.

--- TimeEvoVecMPS/src/TimeEvoVecMPS.jl
+++ TimeEvoVecMPS/src/TimeEvoVecMPS.jl
@@ -17,8 +17,8 @@ include("itensor.jl")
 include("bondgate.jl")
 include("bondop.jl")
 include("callback.jl")
-include("localoperator.jl")
 include("utils.jl")
+include("localoperator.jl")
 
 include("tebd.jl")
 
