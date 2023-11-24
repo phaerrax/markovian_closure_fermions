9647e40ea164d3e9b79e3bf2cd43a679a0a1ec55
Davide Ferracin <davide.ferracin@protonmail.com>
2023-11-24 18:33:19 +0100

    Test TDVP1 and TDVP1vec separately
    Split the `localoperator.jl` test script in two variants, one using the
    TDVP1 algorithm, the other using TDVP1vec. This allows testing the
    `LocalOperator` methods in all (current) use cases.

---
+++ TimeEvoVecMPS/test/localoperator_tdvp1.jl
@@ -0,0 +1,66 @@
+using ITensors
+using PseudomodesTTEDOPA
+using TimeEvoVecMPS
+
+let
+    system_length = 1
+
+    chain_length = 10
+    coups = [0.25/n for n in 1:10]
+    freqs = [0.5/n for n in 1:10]
+    chain_range = system_length .+ (1:chain_length)
+
+    n_sites = chain_length + system_length
+    sites = siteinds("Osc", n_sites; dim=4)
+    psi_t = MPS(sites, [i == 1 ? "1" : "0" for i in 1:n_sites])
+
+    # Schrödinger equation
+    h = OpSum()
+    h += "N", 1
+    h += coups[1], "Asum", 1, "Asum", 2
+    for i in 1:(chain_length-1)
+        h += freqs[i], "N", chain_range[i]
+        h += coups[i+1], "A", chain_range[i], "Adag", chain_range[i+1]
+        h += coups[i+1], "Adag", chain_range[i], "A", chain_range[i+1]
+        end
+        h += freqs[chain_length], "N", chain_range[chain_length]
+
+    H = MPO(h, sites)
+
+    # Enlarge the bond dimensions so that TDVP1 has the possibility to grow
+    # the number of singular values between the bonds.
+    growMPS!(psi_t, 10)
+
+    timestep = 0.02
+    tmax = 10
+
+    operators = [
+        LocalOperator(Dict(1 => "Adag", 2 => "A")),
+        LocalOperator(Dict(3 => "A", 5 => "Adag")),
+        LocalOperator(Dict(4 => "N")),
+    ]
+    cb = LocalOperatorCallback(operators, sites, 5 * timestep)
+
+    tmpfile = tempname()
+    tdvp1!(
+        psi_t,
+        H,
+        timestep,
+        tmax;
+        hermitian=true,
+        normalize=false,
+        callback=cb,
+        progress=false,
+        store_psi0=false,
+        io_file=tmpfile,
+        io_ranks="/dev/null",
+        io_times="/dev/null",
+    )
+
+    results = open(tmpfile, "r") do test_res
+        read(test_res, String)
+    end
+    print(results)
+
+    return nothing
+end
--- TimeEvoVecMPS/test/localoperator_tdvp1vec.jl
+++ TimeEvoVecMPS/test/localoperator_tdvp1vec.jl
@@ -6,34 +6,36 @@ let
     system_length = 1
 
     chain_length = 10
-    coups = [0.25ℯ^(-2n) for n in 1:10]
-    freqs = [0.5 + ℯ^(-4n) for n in 1:10]
+    coups = [0.25/n for n in 1:10]
+    freqs = [0.5 /n for n in 1:10]
     chain_range = system_length .+ (1:chain_length)
 
     n_sites = chain_length + system_length
-    sites = siteinds("vFermion", n_sites)
-    vecρₜ = MPS(sites, [i == 1 ? "Occ" : "Emp" for i in 1:n_sites])
+    sites = siteinds("vOsc", n_sites; dim=4)
+    vecρₜ = MPS(sites, [i == 1 ? "1" : "0" for i in 1:n_sites])
 
     # Master equation
-    ℓ =
-        gkslcommutator("N", 1) +
-        exchange_interaction(sites[1], sites[chain_range[1]]; coupling_constant=coups[1]) +
-        spin_chain(freqs[1:chain_length], coups[2:chain_length], sites[chain_range])
+    ℓ = gkslcommutator("N", 1) + coups[1] * gkslcommutator( "Asum", 1, "Asum", 2)
+    for i in 1:(chain_length-1)
+        ℓ += freqs[i] * gkslcommutator("N", chain_range[i])
+        ℓ += coups[i+1] * gkslcommutator("A", chain_range[i], "Adag", chain_range[i+1])
+        ℓ += coups[i+1] * gkslcommutator("Adag", chain_range[i], "A", chain_range[i+1])
+        end
+        ℓ += freqs[chain_length]* gkslcommutator( "N", chain_range[chain_length])
 
     L = MPO(ℓ, sites)
 
     # Enlarge the bond dimensions so that TDVP1 has the possibility to grow
     # the number of singular values between the bonds.
-    growMPS!(vecρₜ, 10)
+    growMPS!(vecρₜ, 20)
 
-    timestep = 0.01
-    tmax = 1
+    timestep = 0.02
+    tmax = 10
 
     operators = [
         LocalOperator(Dict(1 => "vAdag", 2 => "vA")),
-        LocalOperator(Dict(2 => "vAdag", 1 => "vA")),
-        LocalOperator(Dict(2 => "vA", 5 => "vAdag")),
-        LocalOperator(Dict(1 => "vN")),
+        LocalOperator(Dict(3 => "vA", 5 => "vAdag")),
+        LocalOperator(Dict(4 => "vN")),
     ]
     cb = LocalOperatorCallback(operators, sites, 5 * timestep)
 
@@ -47,7 +49,7 @@ let
         hermitian=false,
         normalize=false,
         callback=cb,
-        progress=true,
+        progress=false,
         store_psi0=false,
         io_file=tmpfile,
         io_ranks="/dev/null",
