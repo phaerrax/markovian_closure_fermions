e3c24c22413de30008efceecaa8a4fe71a76ba77
Davide Ferracin <davide.ferracin@protonmail.com>
2024-01-22 13:54:21 +0100

    Remove "almost zero" regions from sp. dens. domain
    Edit the thermofield script so that it if the spectral density is
    "almost zero" in a certain region, then that region is excluded from the
    computation of the coefficients, so as to avoid convergence issues.

--- chainmapping_thermofield_plots.jl
+++ chainmapping_thermofield_plots.jl
@@ -26,7 +26,7 @@ let
     chain_length = sd_info["number_of_oscillators"]
 
     sdf_plot = plot()
-    xstep(domain; step=0.01) = first(domain):step:last(domain)
+    xstep(domain; step=0.001) = first(domain):step:last(domain)
     for (dom, f) in zip(domains, sdfs)
         plot!(sdf_plot, xstep(dom), f.(xstep(dom)); linestyle=:dash)
     end
@@ -58,11 +58,27 @@ let
         return sum([n(1 / T, μ, ω) * f(ω) for (f, T, μ) in zip(sdfs, Ts, μs)])
     end
 
+    # This function removes from the domain those values for which the function is below
+    # the given threshold. We use it to fix the domain of functions that are "almost zero"
+    # in some region, and would cause trouble in the convergence of the coefficients if
+    # left unchecked.
+    # We deliberately take the interval between the first and last point of the filtered
+    # domain, since we cannot admit gaps in the domain of a Szegő-class function, so
+    # we might as well leave in the result everything in between. If the resulting domain
+    # is such that the function is zero inside it, then the function is "pathological" and
+    # should have never been used for the TEDOPA algorithm in the first place.
+    function exclude_almost_zero(domain, f; threshold=1e-8)
+        list = filter(x -> f(x) > threshold, xstep(domain))
+        min = first(list)
+        max = last(list)
+        return [min; filter(x -> min < x < max, domain); max]
+    end
+
     # To merge the domains: concatenate, sort, then remove duplicates
     # TODO: check if there are gaps in the resulting merged domains.
     merge_domains(domains) = unique(sort(vcat(domains...)))
-    merged_filled_domains = merge_domains(domains_filled)
-    merged_empty_domains = merge_domains(domains_empty)
+    merged_filled_domains = exclude_almost_zero(merge_domains(domains_filled), merged_sdffilled)
+    merged_empty_domains = exclude_almost_zero(merge_domains(domains_empty), merged_sdfempty)
 
     function issingleton(domain)
         if !issorted(domain)
