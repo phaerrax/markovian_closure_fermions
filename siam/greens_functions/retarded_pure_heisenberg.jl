fc8145896e6e68f4fb242b9269a928b6b954e7b8
Davide Ferracin <davide.ferracin@protonmail.com>
2024-04-29 11:59:30 +0300

    Fix vectorization of outer product from pure state
    The density matrix constructed from a pure state in the Green's function
    Heisenberg-picture script was vectorized in the wrong way, since the
    combiner (the way it was usedin the script) only transformed a matrix into
    a vector by stacking rows or columns, but the "v..." site types we
    employ use the Gell-Mann basis, so we need also to change the basis by
    multiplying each tensor by an appropriate matrix.

--- siam/greens_functions/retarded_pure_heisenberg.jl
+++ siam/greens_functions/retarded_pure_heisenberg.jl
@@ -19,19 +19,52 @@ let
     rho_0 = outer(apply(op("a", sites, 1), psi_0), psi_0')
     # Vectorize it and convert it into an MPS. The "combiner" index merges the
     # two matrix indices in a single one, effectively transforming the matrix
-    # into a vector. (I hope the order of the new index values is the right one...)
+    # into a vector. Our vectorized fermion site type works in the Gell-Mann
+    # basis though... we need to implement a change of basis on each site.
+    #
+    # If t = [a b; c d] with index `s` then
+    #   t * combiner(s', s) = [a, c, b, d]  (stacks columns)
+    #   t * combiner(s, s') = [a, b, c, d]  (stacks rows)
+    #
+    # Let's stack by rows, and define
+    #   e[1] = [1 0; 0 0]
+    #   e[2] = [0 1; 0 0]
+    #   e[3] = [0 0; 1 0]
+    #   e[4] = [0 0; 0 1]
+    # so that [a b; c d] = a*e[1] + b*e[2] + c*e[3] + d*e[4].
+    # The 2d Gell-Mann basis (our version of it) is
+    #   f[1] = 1/sqrt(2) [1 0; 0 -1]
+    #   f[2] = 1/sqrt(2) [0 1; 1 0]
+    #   f[3] = 1/sqrt(2) [0 -i; i 0]
+    #   f[4] = 1/sqrt(2) [1 0; 0 1]
+    # and with these definitions we get
+    #   e[i] = sum([U[i,j] * f[j] for j in 1:4])
+    # with
+    #        1  ⎛  1  0  0  1 ⎞
+    #   U = ⸺   ⎜  0  1  i  0 ⎟
+    #       √2  ⎜  0  1 -i  0 ⎟
+    #           ⎝ -1  0  0  1 ⎠
+    # Note that coordinate vectors change basis with the transpose of this matrix,
+    # so we need to multiply each blocks of the MPS by U on the _left_.
+    # We achieve this by defining the tensor with a specific order of the indices.
+    # TODO Write down out how this works, once and for all...
     rho_0_vec = convert(MPS, rho_0)
     for i in 1:length(rho_0_vec)
         rho_0_vec[i] *= combiner(sites[i], sites[i]')
     end
-
+    u_mat = 1 / sqrt(2) .* [
+        1 0 0 1
+        0 1 im 0
+        0 1 -im 0
+        -1 0 0 1
+    ]
+    combined_sites = siteinds(rho_0_vec)
     # The combiner produces an Index of type `(dim=4|id=...|"CMB,Link")`, but we
-    # need a "vFermion" Index on each site instead.
-    replaceind!(rho_0_vec[1], ind(rho_0_vec[1], 2), vsites[1])
-    for i in 2:(length(rho_0_vec) - 1)
-        replaceind!(rho_0_vec[i], ind(rho_0_vec[i], 3), vsites[i])
+    # need a "vFermion" Index on each site instead. Let's also change that.
+    u = [ITensor(ComplexF64, u_mat, combined_sites[i], vsites[i]) for i in 1:length(vsites)]
+    for i in 1:length(rho_0_vec)
+        rho_0_vec[i] *= u[i]
     end
-    replaceind!(rho_0_vec[end], ind(rho_0_vec[end], 2), vsites[end])
     # Now we have a vectorized density matrix which we can use, with our
     # machinery, as an initial state.
 
@@ -82,7 +115,6 @@ let
     )
     targetop = MPS(ComplexF64, vsites, opstrings)
     growMPS!(targetop, parameters["max_bond"])
-    opgrade = -1  # (odd parity)
 
     # The Green's function G(t) = -i ⟨ψ, d(t) d† ψ⟩ can be obtained by looking at the
     # "exp_val_im" column of the output file.
