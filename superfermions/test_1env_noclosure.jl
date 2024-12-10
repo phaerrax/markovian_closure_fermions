0fe8f33ea53c1ee029c32005a1adb9d7ce6324f5
Davide Ferracin <davide.ferracin@protonmail.com>
2024-12-10 14:37:47 +0200

    Read parameters from JSON in superfermion script
    Edit the superfermion script (without Markovian closure) so that it can
    accept parameters from the usual JSON file as well as from the command
    line.  In the future we may use this input strategy as the default one,
    since it adds more flexibility to the currently used one with JSON only.

--- superfermions/test_1env_noclosure.jl
+++ superfermions/test_1env_noclosure.jl
@@ -1,4 +1,4 @@
-using ITensors, ITensorMPS, TimeEvoVecMPS, ArgParse, MKL
+using ITensors, ITensorMPS, TimeEvoVecMPS, ArgParse, MKL, CSV
 
 function enlargelinks_delta(v::MPS, new_d)
     @warn "This function currently doesn't seem to work when QNs are involved."
@@ -187,28 +187,33 @@ end
 
 function simulation(;
     nsystem,
+    system_energy,
+    system_initial_state,
+    sysenvcoupling,
     nenvironment,
+    environment_chain_frequencies,
+    environment_chain_couplings,
     dt,
     tmax,
     maxbonddim,
     io_file=nullfile(),
     io_ranks=nullfile(),
     io_times=nullfile(),
+    operators,
 )
     sites = siteinds("Fermion", 2nsystem + 2nenvironment; conserve_nfparity=true)
-    system = ModeChain(range(; start=1, step=2, length=nsystem), [1], [])
-    altsystem = ModeChain(range(; start=2, step=2, length=nsystem), [1], [])
+    system = ModeChain(range(; start=1, step=2, length=nsystem), [system_energy], [])
+    altsystem = ModeChain(range(; start=2, step=2, length=nsystem), [system_energy], [])
 
-    sysenvcoupling = 1
     environment = ModeChain(
         range(; start=2nsystem + 1, step=2, length=nenvironment),
-        fill(1, nenvironment),
-        fill(1, nenvironment - 1),
+        environment_chain_frequencies,
+        environment_chain_couplings,
     )
     altenvironment = ModeChain(
         range(; start=2nsystem + 2, step=2, length=nenvironment),
-        fill(1, nenvironment),
-        fill(1, nenvironment - 1),
+        environment_chain_frequencies,
+        environment_chain_couplings,
     )
 
     initstate = MPS(sites, n -> n ≤ 2 ? "Occ" : "Emp")
@@ -219,17 +224,6 @@ function simulation(;
 
     L = MPO(-im * ad_h, sites)
 
-    operators = [
-        LocalOperator(Dict(1 => "n"))
-        LocalOperator(Dict(2 => "n"))
-        LocalOperator(Dict(3 => "n"))
-        LocalOperator(Dict(4 => "n"))
-        LocalOperator(Dict(2nsystem + 2nenvironment - 1 => "n"))
-        LocalOperator(Dict(2nsystem + 2nenvironment => "n"))
-        LocalOperator(Dict(1 => "cdag", 3 => "c"))
-        LocalOperator(Dict(3 => "cdag", 1 => "c"))
-        LocalOperator(Dict(2 => "c", 4 => "cdag"))
-    ]
     cb = SuperfermionCallback(operators, sites, dt)
 
     # Prepare the state for TDVP with long-range interactions by artificially increasing
@@ -275,48 +269,73 @@ function parsecommandline()
     @info "Reading parameters from command line"
     s = ArgParseSettings()
     @add_arg_table s begin
+        "--input_parameters", "-i"
+        help = "Path to file with JSON dictionary of input parameters"
+        arg_type = String
         "--system_sites", "--ns"
         help = "Number of system sites"
         arg_type = Int
-        required = true
         "--environment_sites", "--ne"
         help = "Number of environment sites"
         arg_type = Int
-        required = true
         "--time_step", "--dt"
         help = "Time step of the evolution"
         arg_type = Float64
-        required = true
         "--max_time", "--maxt"
         help = "Total physical time of the evolution"
         arg_type = Float64
-        required = true
         "--bond_dimension", "--bdim"
         help = "Bond dimension of the state MPS"
         arg_type = Int
-        "--output_file", "-o"
+        "--name", "-o"
         help = "Path of output file"
         arg_type = String
-        required = true
+    end
+
+    # Load the input JSON file, if present
+    parsedargs_raw = parse_args(s)
+    parsedargs = if haskey(parsedargs_raw, "input_parameters")
+        load_pars(pop!(parsedargs_raw, "input_parameters"))
+    else
+        Dict()
     end
 
     # Convert keys from String to Symbol and remove the ones whose value is `nothing`.
-    parsedargs = Dict()
     for (k, v) in parse_args(s)
-        isnothing(v) || push!(parsedargs, Symbol(k) => v)
+        #isnothing(v) || push!(parsedargs, Symbol(k) => v)
+        isnothing(v) || push!(parsedargs, k => v)
     end
     return parsedargs
 end
 
 function main()
     parsedargs = parsecommandline()
+
+    chain_length = parsedargs["environment_sites"]
+
+    empty_chain_freqs = CSV.File(parsedargs["environment_chain_coefficients"])["freqempty"]
+    empty_chain_coups = CSV.File(parsedargs["environment_chain_coefficients"])["coupempty"]
+
+    ops = LocalOperator[]
+    for (k, v) in parsedargs["observables"]
+        for n in v
+            push!(ops, LocalOperator(Dict(n => k)))
+        end
+    end
+
     simulation(;
-        nsystem=parsedargs[:system_sites],
-        nenvironment=parsedargs[:environment_sites],
-        dt=parsedargs[:time_step],
-        tmax=parsedargs[:max_time],
-        maxbonddim=parsedargs[:bond_dimension],
-        io_file=parsedargs[:output_file],
+        nsystem=parsedargs["system_sites"],
+        system_energy=parsedargs["system_energy"],
+        system_initial_state=parsedargs["system_initial_state"],
+        nenvironment=chain_length,
+        sysenvcoupling=first(empty_chain_coups),
+        environment_chain_frequencies=first(empty_chain_freqs, chain_length),
+        environment_chain_couplings=first(empty_chain_coups[2:end], chain_length - 1),
+        dt=parsedargs["time_step"],
+        tmax=parsedargs["max_time"],
+        maxbonddim=parsedargs["bond_dimension"],
+        io_file=parsedargs["name"] * "_measurements.csv",
+        operators=ops,
     )
 
     return nothing
